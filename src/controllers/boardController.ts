a2664f4a87679f5a69160f9bc172c8471f483982
Alexey Palanevich <alexey.palanevich@satelliteinnovations.io>
2021-12-19 23:55:07 +0300

    docs: contollers

--- src/controllers/boardController.ts
+++ src/controllers/boardController.ts
@@ -4,6 +4,21 @@ import * as boardService from '../services/boardService';
 import { RESPONSE_CODES } from '../helpers/responseCodes';
 import { BoardFields } from '../models/Board.model';
 
+interface IBoardId {
+  /** board id */
+  id: string;
+}
+
+interface IBoardBody {
+  /** board fields */
+  body: BoardFields;
+}
+
+/**
+ * Get all boards
+ * @param _ - request
+ * @param response - response
+ */
 export async function getAllBoards(
   _: IncomingMessage,
   response: ServerResponse
@@ -13,40 +28,72 @@ export async function getAllBoards(
   );
 }
 
+type IGetBoardByIdOptions = IBoardId;
+
+/**
+ * Get Board by id
+ * @param _ - request
+ * @param response - response
+ * @param options - options {@link IGetBoardByIdOptions}
+ */
 export async function getBoardById(
   _: IncomingMessage,
   response: ServerResponse,
-  { id }: { id: string }
+  { id }: IGetBoardByIdOptions
 ) {
   await getResponse(response, RESPONSE_CODES.OK, () =>
     boardService.getBoardById(id)
   );
 }
 
+type ICreateBoardOptions = IBoardBody;
+
+/**
+ * Create new board from object
+ * @param _ - request
+ * @param response - response
+ * @param options - options {@link ICreateBoardOptions}
+ */
 export async function createBoard(
   _: IncomingMessage,
   response: ServerResponse,
-  { body }: { body: BoardFields }
+  { body }: ICreateBoardOptions
 ) {
   await getResponse(response, RESPONSE_CODES.Created, () =>
     boardService.createBoard(body)
   );
 }
 
+interface IUpdateBoardById extends IBoardId, IBoardBody {}
+
+/**
+ * Find Board by id and update it
+ * @param _ - request
+ * @param response - response
+ * @param options - options {@link IUpdateBoardById}
+ */
 export async function updateBoardById(
   _: IncomingMessage,
   response: ServerResponse,
-  { id, body }: { id: string; body: BoardFields }
+  { id, body }: IUpdateBoardById
 ) {
   await getResponse(response, RESPONSE_CODES.OK, () =>
     boardService.updateBoardById(id, body)
   );
 }
 
+type IDeleteBoardById = IBoardId;
+
+/**
+ * Find board by id and delete it
+ * @param _ - request
+ * @param response -response
+ * @param options - options {@link IDeleteBoardById}
+ */
 export async function deleteBoardById(
   _: IncomingMessage,
   response: ServerResponse,
-  { id }: { id: string }
+  { id }: IDeleteBoardById
 ) {
   await getResponse(response, RESPONSE_CODES.Deleted, () =>
     boardService.deleteBoardById(id)
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,8 +2,22 @@ import { IncomingMessage, ServerResponse } from 'node:http';
 import { getResponse } from '../helpers/getResponse';
 import * as userService from '../services/userService';
 import { RESPONSE_CODES } from '../helpers/responseCodes';
-import { User } from '../models/User.model';
+import { UserFields } from '../models/User.model';
 
+interface IUserId {
+  /** user id */
+  id: string;
+}
+interface IUserBody {
+  /** user fields */
+  body: UserFields;
+}
+
+/**
+ * Get all users
+ * @param _ - request
+ * @param response - response
+ */
 export async function getAllUsers(
   _: IncomingMessage,
   response: ServerResponse
@@ -11,40 +25,72 @@ export async function getAllUsers(
   await getResponse(response, RESPONSE_CODES.OK, () => userService.getUsers());
 }
 
+type IGetUserById = IUserId;
+
+/**
+ * Get User by id
+ * @param _ - request
+ * @param response - response
+ * @param options - options {@link IGetUserById}
+ */
 export async function getUserById(
   _: IncomingMessage,
   response: ServerResponse,
-  { id }: { id: string }
+  { id }: IGetUserById
 ) {
   await getResponse(response, RESPONSE_CODES.OK, () =>
     userService.getUserById(id)
   );
 }
 
+type ICreateUser = IUserBody;
+
+/**
+ * Create new user from object
+ * @param _ - request
+ * @param response - response
+ * @param options - options {@link ICreateUser}
+ */
 export async function createUser(
   _: IncomingMessage,
   response: ServerResponse,
-  { body }: { body: Omit<User, 'id'> }
+  { body }: ICreateUser
 ) {
   await getResponse(response, RESPONSE_CODES.Created, () =>
     userService.createUser(body)
   );
 }
 
+interface IUpdateUserById extends IUserId, IUserBody {}
+
+/**
+ * Find user by id and update it
+ * @param _ - request
+ * @param response - response
+ * @param options - options {@link IUpdateUserById}
+ */
 export async function updateUserById(
   _: IncomingMessage,
   response: ServerResponse,
-  { id, body }: { id: string; body: Omit<User, 'id'> }
+  { id, body }: IUpdateUserById
 ) {
   await getResponse(response, RESPONSE_CODES.OK, () =>
     userService.updateUserById(id, body)
   );
 }
 
+type IDeleteTaskById = IUserId;
+
+/**
+ * Find user by id and delete it
+ * @param _ - request
+ * @param response - response
+ * @param options - options {@link IDeleteTaskById}
+ */
 export async function deleteUserById(
   _: IncomingMessage,
   response: ServerResponse,
-  { id }: { id: string }
+  { id }: IDeleteTaskById
 ) {
   await getResponse(response, RESPONSE_CODES.Deleted, () =>
     userService.deleteUserById(id)
